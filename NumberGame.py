76f9e92f12fd44fbaaf5b2ed2636bab3eba144a0
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2022-07-26 10:27:49 -0400

    Create NumberGame.py

---
+++ NumberGame.py
@@ -0,0 +1,31 @@
+import random
+import time
+
+dif = 100
+#Change the value of dif to change the difficulty!
+number = random.randint(1, dif)
+score = float(1)
+
+print("The goal of the game is to guess the number, and get the lowest score possible!")
+time.sleep(1)
+print("You will be told if the number you guessed is greater or lesser than the actual number!")
+time.sleep(1)
+print("Pick a number between 1 and ", dif, "!")
+
+while True:
+    guess = float(input("Enter your guess!"))
+    time.sleep(1)
+    if guess == number:
+        time.sleep(1)
+        print("You win!")
+        time.sleep(1)
+        print("Final score: ", score)
+        break
+    if guess > number:
+        print("The number is lower than", guess)
+        score = score + 1
+        time.sleep(1)
+    if guess < number:
+        print("The number is greater than", guess)
+        score = score + 1
+        time.sleep(1)
